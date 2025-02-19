d6c7898eb7012c6131ac7c74d82fb21b7d46dc32
PranayChowdhury00 <pranaychowdhury00@gmail.com>
2025-02-20 00:38:27 +0600

    finishing the login and register page work

--- src/pages/loginSignIn/Login.jsx
+++ src/pages/loginSignIn/Login.jsx
@@ -1,11 +1,88 @@
-
+import { useContext } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { AuthContext } from "../../AuthProvider/AuthProvider";
+import Swal from "sweetalert2";
 
 const Login = () => {
-    return (
-        <div>
-            
+  const { signInUser } = useContext(AuthContext);
+  const navigate = useNavigate();
+  const handelSubmit = (e) => {
+    e.preventDefault();
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    signInUser(email, password)
+      .then((res) => {
+        Swal.fire({
+          position: "top-end",
+          icon: "success",
+          title: "successful login",
+          showConfirmButton: false,
+          timer: 1500,
+        });
+        console.log(res);
+        e.target.reset();
+        navigate("/");
+      })
+      .catch((err) => {
+        Swal.fire({
+          position: "top-end",
+          icon: "error",
+          title: err.message || "Unsuccessful login",
+          showConfirmButton: false,
+          timer: 1500,
+        });
+      });
+  };
+  return (
+    <div className="hero bg-base-200 min-h-screen">
+      <div className="hero-content flex-col lg:flex-row-reverse">
+        <div className="text-center"></div>
+        <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+          <h1 className="text-3xl font-bold text-center mt-5">Login now!</h1>
+          <form onSubmit={handelSubmit} className="card-body">
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input
+                name="email"
+                type="email"
+                placeholder="email"
+                className="input input-bordered"
+                required
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Password</span>
+              </label>
+              <input
+                name="password"
+                type="password"
+                placeholder="password"
+                className="input input-bordered"
+                required
+              />
+              <label className="label">
+                <a href="#" className="label-text-alt link link-hover">
+                  Forgot password?
+                </a>
+              </label>
+            </div>
+            <div className="form-control mt-6">
+              <button className="btn btn-primary">Login</button>
+            </div>
+          </form>
+          <p className="font-medium text-center py-4">
+            Don't have account?{" "}
+            <Link className="text-red-400 font-semibold" to="/register">
+              Register Now
+            </Link>{" "}
+          </p>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Login;
--- src/pages/loginSignIn/Register.jsx
+++ src/pages/loginSignIn/Register.jsx
@@ -1,9 +1,75 @@
-
+import { useContext } from "react";
+import { AuthContext } from "../../AuthProvider/AuthProvider";
+import { Link, useNavigate } from "react-router-dom";
+import Swal from "sweetalert2";
 
 const Register = () => {
+    const { newUser } = useContext(AuthContext);
+const navigate = useNavigate();
+    const handleOnSubmit = (e) => {
+        e.preventDefault();
+        const name = e.target.name.value;
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+
+        newUser(email, password)
+            .then((res) => {
+                Swal.fire({
+                    position: "top-end",
+                    icon: "success",
+                    title: "successful register",
+                    showConfirmButton: false,
+                    timer: 1500
+                  });
+                console.log(res);
+                e.target.reset();
+                navigate('/')
+            })
+            .catch((err) => {
+                Swal.fire({
+                    position: "top-end",
+                    icon: "error",
+                    title: err.message ||"Unsuccessful register",
+                    showConfirmButton: false,
+                    timer: 1500
+                  });
+            });
+    };
+
     return (
-        <div>
-            
+        <div className="hero bg-base-200 min-h-screen">
+            <div className="hero-content flex-col lg:flex-row-reverse">
+                <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+                    <h1 className="text-3xl font-bold text-center mt-5">Register now!</h1>
+                    <form onSubmit={handleOnSubmit} className="card-body">
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Name</span>
+                            </label>
+                            <input name="name" type="text" placeholder="name" className="input input-bordered" required />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input name="email" type="email" placeholder="email" className="input input-bordered" required />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Password</span>
+                            </label>
+                            <input name="password" type="password" placeholder="password" className="input input-bordered" required />
+                        </div>
+                        <div className="form-control mt-6">
+                            <button className="btn btn-primary">Register</button>
+                        </div>
+                    </form>
+                    <p className="text-center font-medium py-5">
+                        Already have an account?
+                        <Link className="text-green-400 ml-3 font-semibold" to='/login'>Login</Link>
+                    </p>
+                </div>
+            </div>
         </div>
     );
 };
